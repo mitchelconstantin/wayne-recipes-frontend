4579d4a768c3a690e71ae32aece25e34fa5ba84f
Mitchel <mitchconstantin@gmail.com>
2020-06-09 12:29:40 -0500

    add empty margin above top bar to show in transparent mode

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { Box, makeStyles } from '@material-ui/core/';
-import { SmallAppBar } from './SmallAppBar';
-import { LargeAppBar } from './LargeAppBar';
+import React from "react";
+import { Box, makeStyles } from "@material-ui/core/";
+import { SmallAppBar } from "./SmallAppBar";
+import { LargeAppBar } from "./LargeAppBar";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   container: {
-    [theme.breakpoints.down('sm')]: {
-      paddingBottom: '70px'
+    [theme.breakpoints.down("sm")]: {
+      paddingBottom: "70px",
     },
-    [theme.breakpoints.up('md')]: { paddingBottom: '0px' }
-  }
+    [theme.breakpoints.up("md")]: { paddingBottom: "0px" },
+  },
+  coloredBox: {
+    background: "linear-gradient(0.25turn, #f44723, #f56730, #f44723)",
+    marginTop: "-100px",
+    height: "100px",
+  },
 }));
 
 export const Header = () => {
   const classes = useStyles();
   return (
     <Box className={classes.container} displayPrint="none">
+      <div className={classes.coloredBox} />
       <SmallAppBar />
       <LargeAppBar />
     </Box>
