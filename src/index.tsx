3d3240bec67e84d8a42448275afe4190ceccab8e
Mitchel Constantin <mitchelc@ifit.com>
2020-12-26 20:58:15 -0600

    try forcing update

--- src/index.tsx
+++ src/index.tsx
@@ -8,5 +8,13 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-register();
+register({
+  onUpdate: (registration) => {
+    alert("New version available!  Ready to update?");
+    window.location.reload();
+    if (registration && registration.waiting) {
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+    }
+  },
+});
 // serviceWorker.unregister();
