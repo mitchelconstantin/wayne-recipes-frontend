f46f949e612049bde3ce8398b7f2dcd67ff51e73
Mitchel Constantin <mitchelc@ifit.com>
2020-12-28 15:38:16 -0600

    remove some comments and use constants for localstorage strings

--- src/Shared/AppBehaviors.ts
+++ src/Shared/AppBehaviors.ts
@@ -1,32 +1,13 @@
 import { emptyUser, IUser } from "./Types";
 import jwt_decode from "jwt-decode";
 
-// const IS_LOGGED_IN = "isLoggedIn";
-// const USER_EMAIL = "userEmail";
-// const IS_ADMIN = "isAdmin";
-// const IS_OWNER = "isOwner";
 const TOKEN = "token";
 
-// const getLocalStorage = (item: string, isString?: boolean) => {
-//   const result = localStorage.getItem(item);
-//   if (!result) return false;
-//   if (isString) return result;
-//   return JSON.parse(result);
-// };
-
 export const logIn = (token: string) => {
-  // localStorage.setItem(IS_LOGGED_IN, "true");
-  // localStorage.setItem(USER_EMAIL, user.email);
-  // localStorage.setItem(IS_ADMIN, user.isAdmin.toString());
-  // localStorage.setItem(IS_OWNER, user.isOwner.toString());
   localStorage.setItem(TOKEN, token);
 };
 
 export const logOut = () => {
-  // localStorage.setItem(IS_LOGGED_IN, "false");
-  // localStorage.setItem(USER_EMAIL, "");
-  // localStorage.setItem(IS_ADMIN, "false");
-  // localStorage.setItem(IS_OWNER, "false");
   localStorage.removeItem(TOKEN);
   window.location.reload();
 };
--- src/Shared/Hooks/darkTheme.ts
+++ src/Shared/Hooks/darkTheme.ts
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
+const ENABLE_DARK_THEME = "enableDarkTheme";
+
 const getDarkThemeEnabled = (): boolean => {
-  let enabled = localStorage.getItem("enableDarkTheme");
+  let enabled = localStorage.getItem(ENABLE_DARK_THEME);
   if (enabled === null) return false;
   return JSON.parse(enabled);
 };
 
 const toggleTheme = (): void => {
   const toggledValue = JSON.stringify(!getDarkThemeEnabled());
-  localStorage.setItem("enableDarkTheme", toggledValue);
+  localStorage.setItem(ENABLE_DARK_THEME, toggledValue);
 };
 
 export const useDarkThemeEnabled = (): {
