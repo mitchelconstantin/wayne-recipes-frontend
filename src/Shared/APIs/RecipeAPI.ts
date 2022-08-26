42dfcbb195c8a28cf5851377e8fa6a80e1377536
Mitchel Constantin <mitchconstantin@gmail.com>
2022-08-26 10:26:42 -0400

    remove comment

--- src/Shared/APIs/RecipeAPI.ts
+++ src/Shared/APIs/RecipeAPI.ts
@@ -25,7 +25,6 @@ export class RecipeAPI {
     try {
       const { data } = await netlifyInstance.get(`get-one-recipe/${id}`);
       return data.recipe;
-      console.log("data", data);
     } catch (e) {
       console.log("got netlify err", e);
       window.location.href = "/all";
