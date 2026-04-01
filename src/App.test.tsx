2d58d8485f8c486e5a4f1b2363c2483632046f38
Mitchel C <mitchel.constantin@playonsports.com>
2026-03-31 21:15:04 -0400

    bump react

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
 beforeAll(() => {
@@ -16,5 +16,6 @@ beforeAll(() => {
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  const root = createRoot(div);
+  root.render(<App />);
 });
--- src/Shared/Components/customRoutes/CustomRoutes.tsx
+++ src/Shared/Components/customRoutes/CustomRoutes.tsx
@@ -1,18 +1,19 @@
+import React from "react";
 import { Navigate } from "react-router-dom";
 import { isLoggedIn, isAdmin, isOwner } from "../../AppBehaviors";
 
-export const PrivateRoute = ({ element }: { element: JSX.Element }) => {
+export const PrivateRoute = ({ element }: { element: React.JSX.Element }) => {
   return isLoggedIn() ? element : <Navigate to="/all" replace />;
 };
 
-export const PublicRoute = ({ element }: { element: JSX.Element }) => {
+export const PublicRoute = ({ element }: { element: React.JSX.Element }) => {
   return !isLoggedIn() ? element : <Navigate to="/all" replace />;
 };
 
-export const AdminRoute = ({ element }: { element: JSX.Element }) => {
+export const AdminRoute = ({ element }: { element: React.JSX.Element }) => {
   return isAdmin() ? element : <Navigate to="/all" replace />;
 };
 
-export const OwnerRoute = ({ element }: { element: JSX.Element }) => {
+export const OwnerRoute = ({ element }: { element: React.JSX.Element }) => {
   return isOwner() ? element : <Navigate to="/all" replace />;
 };
